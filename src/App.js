9052d46bb37afd287c8d5660efa9cf4083ffe3c9
BrisiaCastrejon <brissmonse@gmail.com>
2018-12-03 15:34:54 -0600

    Visualizacion de mapa y agrega google-map-react

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 import Login from './Login/Login';
 
+import Mapa from './Home/Map';
 // import Favorites from './Home/Favorites'
 
 class App extends Component {
   render() {
     return (
       <div>
-        <Login/>
+        <Login />
+        <Mapa />
         
       </div>
       
