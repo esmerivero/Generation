0599be87dea84a48a9126cca7d3d94ac09571c1a
BrisiaCastrejon <brissmonse@gmail.com>
2018-12-03 17:14:12 -0600

    Arrego de componente, se usa CDN de Bootstrap

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import Login from './Login/Login';
 
 import Mapa from './Home/Map';
-// import Favorites from './Home/Favorites'
+import Favorites from './Home/Favorites'
 
 class App extends Component {
   render() {
@@ -10,7 +10,7 @@ class App extends Component {
       <div>
         <Login />
         <Mapa />
-        
+        <Favorites />
       </div>
       
     );
