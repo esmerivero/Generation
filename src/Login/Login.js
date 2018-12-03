58bed8b6ee8d3508b37d53bef19642131ba12928
lameme <esme.riveroh@gmail.com>
2018-12-03 16:40:40 -0600

    Agregando cdn bootstrap

--- src/Login/Login.js
+++ src/Login/Login.js
@@ -1,12 +1,26 @@
 import React, { Component } from 'react';
 import ButtonLogin from './ButtonLogin';
+import './Login.css';
 
-class ButtonLogout extends Component{
+class Login extends Component{
 
     render(){
         return(
-            <ButtonLogin />
+            <section className="container-login">
+                <form className="form-login">
+                    <div className="form-group">
+                        <label for="input-email">Correo</label>
+                        <input type="email" className="form-control" id="input-email" placeholder="Ingresa tu correo"/>
+                    </div>
+                    <div className="form-group">
+                        <label for="input-pass">Contraseña</label>
+                        <input type="password" className="form-control" id="input-pass" placeholder="Contraseña"/>
+                    </div>
+                    <button type="submit" className="btn btn-primary">Submit</button>
+                </form>
+                <ButtonLogin />
+            </section>
         );
     }
 }
-export default ButtonLogout;
+export default Login;
