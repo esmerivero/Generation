14f0eb0c13c40faddfaecf0719e06982bfc9f303
RaquelGlez <gonzalez.c.raquel@gmail.com>
2018-12-03 19:27:28 -0600

    componente para filtrar ubicacion de empleos

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import React, { Component } from 'react';
 import Login from './Login/Login';
 
 // import Favorites from './Home/Favorites'
+import Search from './Home/Search'
+import Filter from './Home/FilterJobs'
+
 
 class App extends Component {
   render() {
     return (
       <div>
         <Login/>
-        
+        <Search/>
+        <Filter/>
       </div>
       
     );
